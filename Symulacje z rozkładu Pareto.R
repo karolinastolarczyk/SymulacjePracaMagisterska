d19e550587dcdf16e4e0c285fca96332b6b6d74a
karolinastolarczyk <72412134+karolinastolarczyk@users.noreply.github.com>
2023-12-14 21:48:23 +0100

    Add files via upload

--- Symulacje z rozkładu Pareto.R
+++ Symulacje z rozkładu Pareto.R
@@ -1,9 +1,8 @@
 #Ustawiamy ziarno
 set.seed(17)
 
-
-install.packages('EnvStats')
 library('plyr')
+library('EnvStats')
 
 #Ustalamy parametr a 
 a <- 10
@@ -32,6 +31,91 @@ generate_pareto(a, 100000, a_par)
 generate_pareto(a, 1000000, a_par)
 generate_pareto(a, 10000000, a_par)
 
+rpareto(100, a_par, 1)
+
+# 100
+
+x_ppar <- seq(0, 10, by = 0.1) 
+
+K <- c()
+for (x in 1:3001) {
+  K[x] <- generate_pareto(10, 100, a_par)
+} 
+
+K
+length(K)
+v <- c()
+for (x in x_ppar ) {
+  ile<- count(K<=x)
+  v[x*10+1] <- ile[which(ile$x == TRUE),'freq']/length(K)
+} 
+v100<- v
+
+#przesunięcie ze względu na różne koncepcje rozkładu geometrycznego
+v1 <- c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA)
+pgeo <- c(v1, pgeom(x_ppar, prob = 1))
+pgeo <- pgeo[1:101]
+pgeo
+
+plot(x_ppar, v, pch="_", col="blue", xlab="x", ylab="F(x)")  
+points(x_ppar, pgeo, pch="_", col="magenta")
+points(x_ppar, v, pch="_", col="blue")  
+legend(6, 0.9, legend=c("Dystrybuanta 1 ","Dystrybuanta K "),
+       col=c("magenta", "blue"), lty=1:2, cex=0.8)
+
+#500
+
+K <- c()
+for (x in 1:3001) {
+  K[x] <- generate_pareto(10, 500, a_par)
+} 
+
+K
+length(K)
+v <- c()
+for (x in x_ppar ) {
+  ile<- count(K<=x)
+  v[x*10+1] <- ile[which(ile$x == TRUE),'freq']/length(K)
+} 
+v500 <- v
+
+plot(x_ppar, v, pch="_", col="blue", xlab="x", ylab="F(x)") 
+points(x_ppar, pgeo, pch="_", col="magenta")
+points(x_ppar, v, pch="_", col="blue") 
+legend(6, 0.98, legend=c("Dystrybuanta 1 ","Dystrybuanta K "),
+       col=c("magenta", "blue"), lty=1:2, cex=0.8)
+
+#1000
+
+K <- c()
+for (x in 1:3001) {
+  K[x] <- generate_pareto(10, 1000, a_par)
+} 
+
+K
+length(K)
+v <- c()
+for (x in x_ppar ) {
+  ile<- count(K<=x)
+  v[x*10+1] <- ile[which(ile$x == TRUE),'freq']/length(K)
+} 
+v1000 <- v
+
+
+plot(x_ppar, v, pch="_", col="blue", xlab="x", ylab="F(x)") 
+points(x_ppar, pgeo, pch="_", col="magenta")
+points(x_ppar, v, pch="_", col="blue") 
+legend(6, 0.985, legend=c("Dystrybuanta 1 ","Dystrybuanta K "),
+       col=c("magenta", "blue"), lty=1:2, cex=0.8)
+
+#zbiorczy wykres
+plot(x_ppar, v100, pch="_", col="blue", xlab="x", ylab="F(x)") 
+points(x_ppar, pgeo, pch="_", col="magenta")
+points(x_ppar, v100, pch="_", col="blue") 
+points(x_ppar, v500, pch="_", col="green") 
+points(x_ppar, v1000, pch="_", col="red") 
+legend(6, 0.9, legend=c("Dystrybuanta 1 ","Dystrybuanta K100 ","Dystrybuanta K500 ","Dystrybuanta K1000 "),
+       col=c("magenta", "blue", 'green', 'red'), lty=1:2, cex=0.8)
 
 # losowa wielkosc próbki
 #N(t) - proces Poissona 
@@ -67,3 +151,86 @@ generate_pareto_poisson(a, 1000, a_par)
 generate_pareto_poisson(a, 10000, a_par) 
 generate_pareto_poisson(a, 100000, a_par) 
 generate_pareto_poisson(a, 1000000, a_par) 
+
+x_ppar <- seq(0, 10, by = 0.1) 
+
+K <- c()
+for (x in 1:3001) {
+  K[x] <- generate_pareto_poisson(10, 100, a_par)
+} 
+
+K
+length(K)
+v <- c()
+for (x in x_ppar ) {
+  ile<- count(K<=x)
+  v[x*10+1] <- ile[which(ile$x == TRUE),'freq']/length(K)
+} 
+v100<- v
+v100
+#przesunięcie ze względu na różne koncepcje rozkładu geometrycznego
+v1 <- c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA)
+pgeo <- c(v1, pgeom(x_ppar, prob = 1))
+pgeo <- pgeo[1:101]
+pgeo
+x_ppar
+
+plot(x_ppar, v, pch="_", col="blue", xlab="x", ylab="F(x)", ylim=c(0.7,1))  
+points(x_ppar, pgeo, pch="_", col="magenta")
+points(x_ppar, v, pch="_", col="blue")  
+legend(6, 0.9, legend=c("Dystrybuanta 1 ","Dystrybuanta K "),
+       col=c("magenta", "blue"), lty=1:2, cex=0.8)
+
+#500
+
+K <- c()
+for (x in 1:3001) {
+  K[x] <- generate_pareto_poisson(10, 500, a_par)
+} 
+
+K
+length(K)
+v <- c()
+for (x in x_ppar ) {
+  ile<- count(K<=x)
+  v[x*10+1] <- ile[which(ile$x == TRUE),'freq']/length(K)
+} 
+v500 <- v
+
+plot(x_ppar, v, pch="_", col="blue", xlab="x", ylab="F(x)") 
+points(x_ppar, pgeo, pch="_", col="magenta")
+points(x_ppar, v, pch="_", col="blue") 
+legend(6, 0.98, legend=c("Dystrybuanta 1 ","Dystrybuanta K "),
+       col=c("magenta", "blue"), lty=1:2, cex=0.8)
+
+#1000
+
+K <- c()
+for (x in 1:3001) {
+  K[x] <- generate_pareto_poisson(10, 1000, a_par)
+} 
+
+K
+length(K)
+v <- c()
+for (x in x_ppar ) {
+  ile<- count(K<=x)
+  v[x*10+1] <- ile[which(ile$x == TRUE),'freq']/length(K)
+} 
+v1000 <- v
+
+
+plot(x_ppar, v, pch="_", col="blue", xlab="x", ylab="F(x)") 
+points(x_ppar, pgeo, pch="_", col="magenta")
+points(x_ppar, v, pch="_", col="blue") 
+legend(6, 0.985, legend=c("Dystrybuanta 1 ","Dystrybuanta K "),
+       col=c("magenta", "blue"), lty=1:2, cex=0.8)
+
+#zbiorczy wykres
+plot(x_ppar, v100, pch="_", col="blue", xlab="x", ylab="F(x)", ylim=c(0.7,1)) 
+points(x_ppar, pgeo, pch="_", col="magenta")
+points(x_ppar, v500, pch="_", col="green") 
+points(x_ppar, v1000, pch="_", col="red") 
+legend(6, 0.85, legend=c("Dystrybuanta 1 ","Dystrybuanta K100 ","Dystrybuanta K500 ","Dystrybuanta K1000 "),
+       col=c("magenta", "blue", 'green', 'red'), lty=1:2, cex=0.8)
+
--- Symulacje z rozkładu Weibulla.R
+++ Symulacje z rozkładu Weibulla.R
@@ -4,7 +4,7 @@ set.seed(17)
 library('plyr')
 
 #Ustalamy parametr a 
-a <- 0.25
+a <- 0.8
 
 #Ustalamy parametry a i b dla rozkładu Weibulla
 a_par <- 5
@@ -34,6 +34,120 @@ generate_weibull(a, 10000000, a_par)
 generate_weibull(a, 100000000, a_par)
 #generate_weibull(a, 1000000000, a_par)
 
+#100
+
+x_pweibull100 <- seq(0, 100, by = 1) 
+
+K <- c()
+for (x in 1:3001) {
+  K[x] <- generate_weibull(a, 100, a_par)
+} 
+
+K
+length(K)
+v <- c()
+for (x in x_pweibull100 ) {
+  ile<- count(K<=x)
+  v[x+1] <- ile[which(ile$x == TRUE),'freq']/length(K)
+} 
+v100<- v
+v100
+
+
+plot(x_pweibull100, v100, pch="_", col="blue", xlab="x", ylab="F(x)")  
+legend(20, 0.2, legend=c("Dystrybuanta K "),
+       col=c( "blue"), lty=1:2, cex=0.8)
+
+#1000
+
+x_pweibull1000 <- seq(0, 1000, by = 10) 
+
+K <- c()
+for (x in 1:3001) {
+  K[x] <- generate_weibull(a, 1000, a_par)
+} 
+
+K
+length(K)
+v <- c()
+for (x in x_pweibull1000 ) {
+  ile<- count(K<=x)
+  v[x/10+1] <- ile[which(ile$x == TRUE),'freq']/length(K)
+} 
+v1000<- v
+v1000
+
+
+plot(x_pweibull1000, v1000, pch="_", col="blue", xlab="x", ylab="F(x)")  
+legend(200, 0.2, legend=c("Dystrybuanta K "),
+       col=c( "blue"), lty=1:2, cex=0.8)
+
+#10 000
+
+x_pweibull10000 <- seq(0, 10000, by = 100) 
+
+K <- c()
+for (x in 1:3001) {
+  K[x] <- generate_weibull(a, 10000, a_par)
+} 
+
+K
+length(K)
+v <- c()
+for (x in x_pweibull10000 ) {
+  ile<- count(K<=x)
+  v[x/100+1] <- ile[which(ile$x == TRUE),'freq']/length(K)
+} 
+v1000<- v
+v1000
+
+
+plot(x_pweibull10000, v1000, pch="_", col="blue", xlab="x", ylab="F(x)")  
+legend(7000, 0.2, legend=c("Dystrybuanta K "),
+       col=c( "blue"), lty=1:2, cex=0.8)
+
+#100 000
+
+x_pweibull100000 <- seq(0, 100000, by = 1000) 
+
+K <- c()
+for (x in 1:3001) {
+  K[x] <- generate_weibull(a, 100000, a_par)
+} 
+
+K
+length(K)
+v <- c()
+for (x in x_pweibull100000 ) {
+  ile<- count(K<=x)
+  v[x/1000+1] <- ile[which(ile$x == TRUE),'freq']/length(K)
+} 
+v100000<- v
+v100000
+options(scipen=10000)
+
+par(mfrow=c(1,1))
+plot(x_pweibull100000, v100000, pch="_", col="blue", xlab="x", ylab="F(x)")  
+legend(15000, 0.2, legend=c("Dystrybuanta K100000 "),
+       col=c( "blue"), lty=1:2, cex=0.8)
+
+
+attach(mtcars)
+
+par(mfrow=c(2,2))
+
+plot(x_pweibull100, v100, pch="_", col="blue", xlab="x", ylab="F(x)")  
+legend(5, 0.8, legend=c("Dystrybuanta K100 "),
+       col=c( "blue"), lty=1:2, cex=0.6)
+plot(x_pweibull1000, v1000, pch="_", col="blue", xlab="x", ylab="F(x)") 
+legend(50, 0.8, legend=c("Dystrybuanta K1000 "),
+       col=c( "blue"), lty=1:2, cex=0.6)
+plot(x_pweibull10000, v10000, pch="_", col="blue", xlab="x", ylab="F(x)")  
+legend(500, 0.8, legend=c("Dystrybuanta K10000 "),
+       col=c( "blue"), lty=1:2, cex=0.6)
+plot(x_pweibull100000, v100000, pch="_", col="blue", xlab="x", ylab="F(x)")  
+legend(5000, 0.8, legend=c("Dystrybuanta K100000 "),
+       col=c( "blue"), lty=1:2, cex=0.6)
 
 # losowa wielkosc próbki
 #N(t) - proces Poissona 
@@ -69,3 +183,119 @@ generate_weibull_poisson(a, 1000, a_par)
 generate_weibull_poisson(a, 10000, a_par) 
 generate_weibull_poisson(a, 100000, a_par) 
 generate_weibull_poisson(a, 1000000, a_par) 
+
+#100
+
+x_pweibull100 <- seq(0, 100, by = 1) 
+
+K <- c()
+for (x in 1:3001) {
+  K[x] <- generate_weibull_poisson(a, 100, a_par)
+} 
+
+K
+length(K)
+v <- c()
+for (x in x_pweibull100 ) {
+  ile<- count(K<=x)
+  v[x+1] <- ile[which(ile$x == TRUE),'freq']/length(K)
+} 
+v100<- v
+v100
+
+
+plot(x_pweibull100, v100, pch="_", col="blue", xlab="x", ylab="F(x)")  
+legend(20, 0.2, legend=c("Dystrybuanta K "),
+       col=c( "blue"), lty=1:2, cex=0.8)
+
+#1000
+
+x_pweibull1000 <- seq(0, 1000, by = 10) 
+
+K <- c()
+for (x in 1:3001) {
+  K[x] <- generate_weibull_poisson(a, 1000, a_par)
+} 
+
+K
+length(K)
+v <- c()
+for (x in x_pweibull1000 ) {
+  ile<- count(K<=x)
+  v[x/10+1] <- ile[which(ile$x == TRUE),'freq']/length(K)
+} 
+v1000<- v
+v1000
+
+
+plot(x_pweibull1000, v1000, pch="_", col="blue", xlab="x", ylab="F(x)")  
+legend(200, 0.2, legend=c("Dystrybuanta K "),
+       col=c( "blue"), lty=1:2, cex=0.8)
+
+#10 000
+
+x_pweibull10000 <- seq(0, 10000, by = 100) 
+
+K <- c()
+for (x in 1:3001) {
+  K[x] <- generate_weibull_poisson(a, 10000, a_par)
+} 
+
+K
+length(K)
+v <- c()
+for (x in x_pweibull10000 ) {
+  ile<- count(K<=x)
+  v[x/100+1] <- ile[which(ile$x == TRUE),'freq']/length(K)
+} 
+v10000<- v
+v10000
+
+
+plot(x_pweibull10000, v10000, pch="_", col="blue", xlab="x", ylab="F(x)")  
+legend(7000, 0.2, legend=c("Dystrybuanta K "),
+       col=c( "blue"), lty=1:2, cex=0.8)
+
+#10
+
+x_pweibull10 <- seq(0, 10, by = 0.1) 
+
+K <- c()
+for (x in 1:3001) {
+  K[x] <- generate_weibull_poisson(a, 10, a_par)
+} 
+
+K
+length(K)
+v <- c()
+for (x in x_pweibull10 ) {
+  ile<- count(K<=x)
+  v[x*10+1] <- ile[which(ile$x == TRUE),'freq']/length(K)
+} 
+v10<- v
+v10
+options(scipen=10000)
+
+par(mfrow=c(1,1))
+plot(x_pweibull10, v10, pch="_", col="blue", xlab="x", ylab="F(x)")  
+legend(15000, 0.2, legend=c("Dystrybuanta K100000 "),
+       col=c( "blue"), lty=1:2, cex=0.8)
+
+
+
+attach(mtcars)
+
+par(mfrow=c(2,2))
+
+plot(x_pweibull10, v10, pch="_", col="blue", xlab="x", ylab="F(x)")  
+legend(5, 0.4, legend=c("Dystrybuanta K10 "),
+       col=c( "blue"), lty=1:2, cex=0.6)
+plot(x_pweibull100, v100, pch="_", col="blue", xlab="x", ylab="F(x)")  
+legend(50, 0.4, legend=c("Dystrybuanta K100 "),
+       col=c( "blue"), lty=1:2, cex=0.6)
+plot(x_pweibull1000, v1000, pch="_", col="blue", xlab="x", ylab="F(x)") 
+legend(500, 0.4, legend=c("Dystrybuanta K1000 "),
+       col=c( "blue"), lty=1:2, cex=0.6)
+plot(x_pweibull10000, v10000, pch="_", col="blue", xlab="x", ylab="F(x)")  
+legend(4800, 0.4, legend=c("Dystrybuanta K10000 "),
+       col=c( "blue"), lty=1:2, cex=0.6)
